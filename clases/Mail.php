0a828140961aae49031e86a6278df1e751009f7c
Servidor <mail@servidor.com>
2012-08-09 17:59:38 -0500

    correos smtp

--- clases/Mail.php
+++ clases/Mail.php
@@ -22,24 +22,22 @@ class Mail
 		$this->MAIL = new PHPMailer();
 		$this->MAIL->IsSMTP();
 		$this->MAIL->SMTPAuth = true;
-		$this->MAIL->Host = $this->SERVIDOR; // SMTP a utilizar. Por ej. smtp.elserver.com
+		$this->MAIL->Host = "ssl://" . $this->SERVIDOR; // SMTP a utilizar. Por ej. smtp.elserver.com
 		$this->MAIL->Username = $this->USUARIO; // Correo completo a utilizar
 		$this->MAIL->Password = $this->PASSWORD; // Contraseña
 		$this->MAIL->Port = $this->PUERTO; // Puerto a utilizar
 	}
 
 
-
 	function EnviarCorreo($nombre, $desde, $para, $asunto, $mensaje)
 	{
-		$this->MAIL->From = $desde; // Desde donde enviamos (Para mostrar)
-		$this->MAIL->FromName = $nombre;
-
+		$this->MAIL->SetFrom($desde, $nombre); // Desde donde enviamos (Para mostrar)
+		$this->MAIL->Sender=$desde;
 		//Estas dos líneas, cumplirían la función de encabezado (En mail() usado de esta forma: “From: Nombre <correo@dominio.com>”) de //correo.
 		$this->MAIL->AddAddress($para); // Esta es la dirección a donde enviamos
 		$this->MAIL->IsHTML(true); // El correo se envía como HTML
 		$this->MAIL->Subject = $asunto; // Este es el titulo del email.
-		$this->MAIL->Body = $mensaje; // Mensaje a enviar
+		$this->MAIL->MsgHTML($mensaje);
 		$exito = $this->MAIL->Send(); // Envía el correo.
 
 		//También podríamos agregar simples verificaciones para saber si se envió:
