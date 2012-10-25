5b767c9d5d6068ff54e8a63a67f2e12a0bef383e
Servidor <mail@servidor.com>
2012-10-24 20:43:24 -0500

    ok

--- verificaSesion.php
+++ verificaSesion.php
@@ -1,11 +1,24 @@
 <?php
+	include_once("clases/Sesion.php");
+	include_once("clases/Usuarios.php");
+	include_once("clases/Cookie.php");		
 
-	$RESULTADO = array();
+	session_start();
+	Sesion::forzarAbrirSesion();
+	
+	$mensaje = "OFF";
+	
+	if(isset($_SESSION['ID']))
+		$mensaje = base64_encode($_SESSION["ID"]);
+	
+	/*$RESULTADO = array();
 	if(isset($_SESSION['ID']))
 		$RESULTADO['STATUS'] = "ON";
 	else
-		$RESULTADO['STATUS'] = "OFF";
+		$RESULTADO['STATUS'] = "OFF";*/
 
-	echo json_encode($RESULTADO);
+	header("Content-Type: text/javascript");
+	echo $_GET['callback'] . "(" . json_encode($mensaje) . ")";
+	//echo json_encode($mensaje);
 
 ?>
